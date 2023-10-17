4c4afc054b0438137bf3345bd8fd0f9c4123a0e5
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-10-17 09:48:45 +0300

    solution

---
+++ asphalt-patches/solution.py
@@ -0,0 +1,16 @@
+def solution(S):
+    count = 0
+    N = len(S)
+    pos = 0
+    while i < N:
+        if S[i] == 'X':
+            
+        
+
+str = '.X.X.XX....X'
+print(solution(str))
+print(solution(".X..X"))        # Output: 2
+print(solution("X.XXXXX.X."))   # Output: 3
+print(solution("XX.XXX.."))     # Output: 2
+print(solution("XXXX"))         # Output: 2
+print(solution('.X...XX'))      # Output: 2
