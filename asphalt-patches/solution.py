c9f4fcc62f8f8b5ece8a8adfbb4a635b5a5ef95a
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-10-17 22:45:56 +0300

    dice roller folder

--- asphalt-patches/solution.py
+++ asphalt-patches/solution.py
@@ -1,10 +1,13 @@
 def solution(S):
     count = 0
     N = len(S)
-    pos = 0
-    while i < N:
-        if S[i] == 'X':
-            
+    i = 0
+    while i < N:   
+         if S[i] == "X":
+            count +=1
+            i += 2
+         else: i+=1
+    return count
         
 
 str = '.X.X.XX....X'
